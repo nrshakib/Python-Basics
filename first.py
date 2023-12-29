6d07fd462fa0d45c5fc70e7f35b37e87b60c78fe
NRS\nrsha <nrshakib007@gmail.com>
2023-12-29 16:51:50 +0600

    Data Types

--- first.py
+++ first.py
@@ -1,3 +1,31 @@
+# Print a statement
 print('hello world')
+
+# Print a number(int/float)
 print(99)
+
+# Print a number(complex)
+print(5+10j)
+
+# Print a string
+# With Escape Sequence
 print("Hello \'Shakib'")
+
+
+# Data Types
+
+a = 1 #(int,float)
+print(a)
+print("Type of a is", type(a))
+b= True #(boolean)
+print(b)
+print("Type of b is", type(b))
+c= "Shakib" #(string)
+print(c)
+print("Type of c is", type(c))
+d = None #(none)
+print(d)
+print("Type of d is", type(d))
+e = 5+10j #(cpmplex)
+print(e)
+print("Type of e is", type(e))
