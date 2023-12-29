c8935355f7723c37d546de4d25c9aefbe8cb207c
NRS\nrsha <nrshakib007@gmail.com>
2023-12-29 17:51:50 +0600

    arithmatic operators

---
+++ calculator.py
@@ -0,0 +1,18 @@
+num1 = 10
+num2 = 3
+
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+mod = num1 % num2
+exp = num1 ** num2
+floorDivision = num1 // num2
+
+print(add)
+print(sub)
+print(mul) 
+print(div)
+print(mod)
+print(exp)
+print(floorDivision)
