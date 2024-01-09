7ca10b5def63d7a29a6185cf68cf0b27eb05c4d7
NRS\nrsha <nrshakib007@gmail.com>
2024-01-09 17:22:56 +0600

    Logical Operators

---
+++ Operators.py
@@ -0,0 +1,10 @@
+# Logical Operator
+# AND, OR, NOT
+
+good_Batter = True
+good_Bowler = False
+
+if good_Batter and not good_Bowler:
+    print('He is an all rounder')
+else:
+    print('He is not an all rounder')
