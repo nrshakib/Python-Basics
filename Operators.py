9b4bbe2c97893e1bd1580b7914a8303f7bda41ab
NRS\nrsha <nrshakib007@gmail.com>
2024-01-09 17:29:04 +0600

    Comparison Operators

--- Operators.py
+++ Operators.py
@@ -8,3 +8,15 @@ if good_Batter and not good_Bowler:
     print('He is an all rounder')
 else:
     print('He is not an all rounder')
+
+
+# Comparison Operator
+    
+name = 'Python'
+
+if len(name) < 3:
+    print("Name Must Be Of Minimum 3 Characters")
+elif len(name) > 10:
+    print("Name can be of 10 characters max")
+else:
+    print("Everything is Okay")
