ed395e3c43d00ffdde577d3f474e2ec8693a4fcb
NRS\nrsha <nrshakib007@gmail.com>
2024-01-11 19:11:37 +0600

    List in Python

---
+++ List.py
@@ -0,0 +1,14 @@
+'''
+    List in Python is almost same as Array in Js, but List can have multiple data of different data types
+    where an Array can hold multiple data of single data type.
+'''
+
+names = ['MD', 'Nadim', 'Rijvi' ,'Shakib', 'Prodhan']
+print(names[3]) # printing value acccording to the index
+names[0] = 'Mohammad' # changing value of item according to index
+print(names)
+
+## printing multiple values of diffetent data types
+## name?, age?, engineer?, 
+profile = [ 'Shakib' , 27, True]
+print(profile)
