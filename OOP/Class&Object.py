836dc0aa2ca8b710ef30facbb3347c45d1c851bc
NRS\nrsha <nrshakib007@gmail.com>
2024-02-12 16:04:06 +0600

    Class & Objects in OOP

---
+++ OOP/Class&Object.py
@@ -0,0 +1,14 @@
+class Student:
+    roll = ''
+    gpa= ''
+
+
+Shakib = Student()
+Shakib.roll = 75
+Shakib.gpa = 3.11
+print(f'Roll: {Shakib.roll},GPA:{Shakib.gpa}')
+
+Rijvi = Student()
+Rijvi.roll = 10
+Rijvi.gpa = 3.99
+print(f'Roll: {Rijvi.roll},GPA:{Rijvi.gpa}')
